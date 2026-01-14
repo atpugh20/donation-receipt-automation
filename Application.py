540bfa3e028c33915881be3bef08fb84c38fdbee
Alex <133706872+atpugh20@users.noreply.github.com>
2026-01-13 17:14:44 -0800

    Feat: handled a lot of the CRM clicking

--- Application.py
+++ Application.py
@@ -1,8 +1,16 @@
+from WebDriver import WebDriver
+
 class Application:
     def __init__(self):
         print("Building app...")
+
+        self.webDriver = WebDriver()
 
         print("App build.")
 
     def Run(self):
         print("Running app...")
+
+        self.webDriver.OpenCRM()
+
+        print("App completed running.") 
--- WebDriver.py
+++ WebDriver.py
@@ -1,4 +1,5 @@
 import os
+import time
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -17,18 +18,70 @@ class WebDriver:
         self.crm_url = os.getenv("CRM_URL")
         self.fundraising_url = os.getenv("FUNDRAISING_URL")
 
+        self.targetEmail = "Donation Receipt Copy (Copy)"
+        self.createdBy = "BloomerangFundraisingIntegration-Dillon Phillips"
+
         self.driver1 = webdriver.Firefox()
 
         print("Web Driver Built.")
 
-    def OpenCRM() -> None:
+    def OpenCRM(self) -> None:
+        self.driver1.get(self.crm_url)
+        time.sleep(1)
+        self.driver1.find_element(By.ID, "usernameField").send_keys(self.username)
+        self.driver1.find_element(By.ID, "passwordField").send_keys(self.password)
+        time.sleep(1)
+        self.driver1.find_element(By.ID, "login-submit").click()
+        time.sleep(2)
+        self.driver1.find_element(By.ID, "communications-sub-navigation-label").click()
+        time.sleep(1)
+        self.driver1.find_element(By.ID, "emails-navigation-link-label").click()
+        time.sleep(1)
+        self.ClickFromClass("email-name-data", self.targetEmail)
+
+        time.sleep(1)
+
+        self.driver1.find_element(By.ID, "recipients-tab").click()
+        time.sleep(1)
+        self.driver1.find_element(By.CLASS_NAME, "filter-tag-drag-source").click()
+        time.sleep(1)
+
+        self.ClickFromClass("name-button", "Acknowledgement Status")
+
+        time.sleep(2)
+
+        self.driver1.find_element(By.CLASS_NAME, "rw-widget-input").click()
+        self.ClickFromClass("multiselect-item", "No")
+        self.driver1.find_element(By.CLASS_NAME, "btn-primary").click()
+        time.sleep(1)
+        self.ClickFromClass("cell", "And...")
+        self.ClickFromClass("name-button", "Created By")
+        time.sleep(1)
+        self.ClickFromClass("name-button", "Created By")
+        time.sleep(1)
+        self.driver1.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div/div[1]/div/div/div[4]/div[4]/div/div/section/div/div[2]/div/div/div/div/section/div/div/div[2]/div/div/input").send_keys(self.createdBy)
+        self.driver1.find_element(By.CLASS_NAME, "btn-primary").click()
+
+        time.sleep(1)
+        self.driver1.find_element(By.ID, "email-save-and-button").click()
+        self.driver1.find_element(By.ID, "email-save-and-button-0").click()
+
+
+
+    def OpenFundraising(self) -> None:
         pass
 
-    def OpenFundraising() -> None:
+    def ExportFromFundraising(self) -> None:
         pass
 
-    def ExportFromFundraising() -> None:
+    def SendReceipts(self) -> None:
         pass
 
-    def SendReceipts() -> None:
-        pass
+    def ClickFromClass(self, className: str, targetText: str) -> None:
+        collection = self.driver1.find_elements(By.CLASS_NAME, className) 
+        
+        for e in collection:
+            if e.text == targetText:
+                print("Clicking on", targetText)
+                e.click()
+                break
